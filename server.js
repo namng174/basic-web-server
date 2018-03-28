bbf4bb68faf92da40409983dfb50ac89648a4307
Namg Ng <namng174@gmail.com>
2018-03-28 23:46:29 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const hbs = require('express-hbs')
+const fs = require('fs')
+
+var app = express()
+
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials',
+}))
+app.set('views', __dirname + '/views')
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase() 
+})
+
+app.use((req, res, next) => {
+    var now = new Date().toString()
+    var log = `${now} : ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to append to server.log')
+        }
+    })
+    next()
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs')
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage: 'Welcome to my Website'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle the request'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
